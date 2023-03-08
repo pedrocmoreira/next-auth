22f7aa3e149a04fdd2ede9c416834f72944c8db1
Pedro Moreira <pedro.henrique@agenciaalldigital.com>
2023-03-08 12:16:43 -0300

    feat: criação e configuração do projeto e configuração de contexto

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+
+import { AuthProvider } from '@/contexts/AuthContext';
+
+import '../styles/global.css';
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
